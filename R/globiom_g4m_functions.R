d4fbff1e331e2be191db048671a5d85cd9fe6f3c
andreyaugustynczik <79080780+andreyaugustynczik@users.noreply.github.com>
2021-05-25 15:58:38 +0200

    Add the function to run the final post-processing script using G4M and GLOBIOM outputs

--- R/globiom_g4m_functions.R
+++ R/globiom_g4m_functions.R
@@ -286,3 +286,99 @@ run_downscaling <- function(cd)
   
   setwd(cd)
 }
+
+
+
+
+
+#' Call the final GLOBIOM run. The function reads, edits and executes the 8_merged_output.gms script
+#' to generate reports for IAMC
+run_globiom_final <- function(cd){
+  
+  # Define wd
+  WD <- str_glue(cd,"/",WD_GLOBIOM,"/")
+  setwd(WD)
+
+  # Configure merged output file
+  tempString <- read_lines("./Model/8_merge_output_tmp.gms")
+  tempString <- str_replace(tempString,"\\$set\\s+rep_g4m\\s+[:print:]+",str_glue("$set rep_g4m ",REPORTING_G4M_FINAL))
+  tempString <- str_replace(tempString,"\\$set\\s+rep_iamc_glo\\s+[:print:]+",str_glue("$set rep_iamc_glo ",REPORTING_IAMC_FINAL))
+  tempString <- str_replace(tempString,"\\$set\\s+rep_iamc_g4m\\s+[:print:]+",str_glue("$set rep_iamc_g4m ",REPORTING_IAMC_G4M_FINAL))
+  tempString <- str_replace(tempString,"\\$set\\s+g4mfile\\s+[:print:]+",str_glue("$set g4mfile ",G4M_FEEDBACK_FILE))
+  tempString <- str_replace(tempString,"\\$include\\s+8c_rep_iamc_g4m","$include 8c_rep_iamc_g4m_tmp")
+  
+  # Save file 
+  write_lines(tempString, "./Model/8_merge_output_tmp.gms")
+  
+  # read in G4M output file
+  g4m_output <- read.csv(str_glue(PATH_FOR_FEEDBACK,G4M_FEEDBACK_FILE) # Will be mofidied in the future to work with gdx files
+                         , header=FALSE)
+  
+  # Define G4M scenarios
+  scen <- unique(g4m_output$V2)
+  scen <- scen[which(scen != "")]
+  
+  # Split G4M scenarios into GLOBIOM dimensions
+  scen_globiom_map <- str_split_fixed(scen,"__",3)
+  
+  # Check if scenario name must be treated as string
+  if (any(str_detect(scen,"%"))) special_char <- TRUE
+  if(special_char)  scen <- unlist(lapply(scen,function(x) str_glue("\"",x,"\"")))
+  
+  # Define column indices of Macro, Bioen and IEA scenarios
+  macro_idx <- which(str_detect(scen_globiom_map[1,],"SSP"))
+  iea_idx <- which(str_detect(scen_globiom_map[1,],"RCP"))
+  bioen_idx <- 6 - macro_idx - iea_idx
+  
+  if(special_char) scen_globiom_map[,bioen_idx] <- unlist(lapply(scen_globiom_map[,bioen_idx],function(x) str_glue("\"",x,"\"")))
+  
+  # Create scenario mapping string
+  for (i in 1:length(scen)){
+    
+    if (i==1) map_string <- str_glue(scen[i]," . ", scen_globiom_map[i,macro_idx], " . ",
+                                     scen_globiom_map[i,bioen_idx], " . ",scen_globiom_map[i,iea_idx])
+    map_string <- c(map_string,str_glue(scen[i]," . ", scen_globiom_map[i,macro_idx], " . ",
+                                        scen_globiom_map[i,bioen_idx], " . ",scen_globiom_map[i,iea_idx]))
+  }
+  
+  
+  # Define sets for mapping
+  g4m_globiom_map <- c("SETS","G4MScen2","/",scen,"/","","G4M_SCEN_MAP(G4MScen2,*,*,*)",
+                  "/",map_string,"/",";")
+  
+  # Configure merged output file
+  tempString <- read_file("./Model/8c_rep_iamc_g4m.gms")
+  tempString <- str_replace(tempString,regex('SET[[:print:]*|[\r\n]*]*G4M_SCEN_MAP[[:print:]*|[\r\n]*]*/[\r\n\\s]+;'),
+                            str_c(g4m_globiom_map,collapse="\n"))
+  
+  path_for_feedback2 <- str_replace_all(PATH_FOR_FEEDBACK,"/","%X%")
+  
+  tempString <- str_replace(tempString,"\\$include\\s+[:print:]*X[:print:]*",
+                           str_glue("$include ",path_for_feedback2,G4M_FEEDBACK_FILE))
+  
+  # Save file 
+  write_lines(tempString, "./Model/8c_rep_iamc_g4m_tmp.gms")
+  
+  # Change wd to run post-processing file
+  wd_model <- str_glue(WD,"Model/")
+  setwd(wd_model)
+  
+  # Run post-processing script
+  rc <- tryCatch(
+    system("gams 8_merge_output_tmp.gms"),
+    error=function(e) e
+  )
+  if(rc != 0){
+    setwd(WD)
+    stop("Bad return from gams")
+  }
+  
+  # Return to previous wd
+  setwd(cd)
+  }
+  
+  
+  # Return to previous wd
+  setwd(cd)
+}
+
