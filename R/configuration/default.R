1de63f410825c8fb6e8bd3b1678006bc4837737c
andreyaugustynczik <augustynczik@iiasa.ac.at>
2021-07-14 10:21:16 +0200

    Revert "Merge branch 'main' of https://github.com/iiasa/GLOBIOM-G4M-link into main"
    This reverts commit 3181288e06262e7a86c7a1cd3c08f7e6e2c8f40f, reversing
    changes made to 1724af0c7e284eb7569ea2fbf7ce0579b2489bb2.

--- R/configuration/default.R
+++ R/configuration/default.R
@@ -1,3 +1,7 @@
+################################################################################
+#                     DEFAULT CONFIGURATION PARAMETERS
+################################################################################
+
 # 1st block - Initial GLOBIOM run
 #-------------------------------------------------------------------------------
 
@@ -10,11 +14,11 @@ GLOBIOM_SCEN_FILE = "6_scenarios.gms"
 
 # Post-processing script configuration (8_merged_output)
 LIMPOPO_RUN = "yes" # Run on limpopo yes/no
-#RESOLUTION = "region37" # set GLOBIOM region "resolution"
-DATE_LABEL = format(Sys.Date(), "%d%m%Y") # date of run
-REPORTING_G4M = "yes" # reporting to G4M yes/no
-REPORTING_IAMC = "yes" # reporting to IAMC yes/no
-REPORTING_IAMC_G4M = "no" # reporting G4M to IAMC REGIONAL_AG/no
+#RESOLUTION = "region37" # Set GLOBIOM region "resolution"
+DATE_LABEL = format(Sys.Date(), "%d%m%Y") # Date of run
+REPORTING_G4M = "yes" #Reporting to G4M yes/no
+REPORTING_IAMC = "yes" #Reporting to IAMC yes/no
+REPORTING_IAMC_G4M = "no" # Reporting G4M to IAMC REGIONAL_AG/no
 REGIONAL_AG = "ggi" # regional aggregation level
 PATH_FOR_DOWNSCALING = "H:/Downscaling/Model/input/" # path to save gdx for downscaling
 
@@ -34,14 +38,14 @@ RESOLUTION_DOWNSCALING = 37 # number of regions specified in the downscaling
 WD_G4M = "G4M" # optional, working directory for G4M relative to current path
 G4M_EXE = "g4m_EPA_07052021.exe" # name of G4M executable
 CO2_PRICE = -1 # co2 price for G4M run, -1 if read form a file or actual price otherwise
-SCENARIOS_FOR_G4M = "70:87" # full set or subset of downscaled scenarios
+SCENARIOS_FOR_G4M = "70:87" # full set or subset of downscaled scenarios 
 #-------------------------------------------------------------------------------
 
 # 3rd block - final GLOBIOM run
 #-------------------------------------------------------------------------------
-PATH_FOR_FEEDBACK = stringr::str_glue("/out/{PROJECT}_{DATE_LABEL}/") # path for G4M output file
-G4M_FEEDBACK_FILE = "tabs_gui_FAOFRA2015CRF_CSIRO_t14_SSP2_EPA_07052021_final_csv_test3.csv" # name of G4M output file
-REPORTING_G4M_FINAL = "no" # reporting to G4M yes/no
-REPORTING_IAMC_FINAL = "yes" # reporting to IAMC yes/no
-REPORTING_IAMC_G4M_FINAL = "yes" #  reporting G4M to IAMC REGIONAL_AG/no
+PATH_FOR_FEEDBACK = stringr::str_glue("/out/{PROJECT}_{DATE_LABEL}/") #Path for G4M output file
+G4M_FEEDBACK_FILE = "tabs_gui_FAOFRA2015CRF_CSIRO_t14_SSP2_EPA_07052021_final_csv_test3.csv" #Name of G4M output file
+REPORTING_G4M_FINAL = "no" #Reporting to G4M yes/no
+REPORTING_IAMC_FINAL = "yes" #Reporting to IAMC yes/no
+REPORTING_IAMC_G4M_FINAL = "yes" # Reporting G4M to IAMC REGIONAL_AG/no
 #-------------------------------------------------------------------------------
