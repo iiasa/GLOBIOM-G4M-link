c8df6ee3061289e23fec1676bcd4a3e1873a82fc
andreyaugustynczik <augustynczik@iiasa.ac.at>
2021-10-20 11:16:06 +0200

    Remove deprecated functions

--- R/transfer.R
+++ R/transfer.R
@@ -1,51 +1,6 @@
 # Functions for transferring data between models
 
-#' Function to transfer the downscaling output to G4M folder. Merges the downscaled
-#' regions if required
-#'
-#' @param cluster_nr_downscaling Cluster sequence number of prior downscaling HTCondor submission
-merge_and_transfer2 <- function(cluster_nr_downscaling) {
-  # Transfer gdx to G4M folder - in case files were merged on limpopo
-  if (MERGE_GDX_DOWNSCALING){
-
-    # Save merged output to G4M folder
-    f <- str_glue(WD_DOWNSCALING,"/gdx/downscaled_{PROJECT}_{cluster_nr_downscaling}_merged.gdx")
-    file_copy(f, path(CD, PATH_FOR_G4M, str_glue("downscaled_output_{PROJECT}_{DATE_LABEL}.gdx")), overwrite = TRUE)
-  } else {
-
-    # Get scenario mapping and indices
-    scenario_mapping <- get_mapping()
-    scenarios_idx <- scenario_mapping$ScenNr[which(scenario_mapping$ScenLoop %in% SCENARIOS_FOR_DOWNSCALING)]
-
-    # Extract land cover table for G4M
-    for (i in 1:length(scenarios_idx)){
-
-      # Get scenario number
-      s_list <-  sprintf("%06d", scenarios_idx[i])
-
-      # Read data for G4M
-      if (i==1) {
-        out <- gdx(path(CD,WD_DOWNSCALING,"gdx", str_glue("{GDX_OUTPUT_NAME}_{PROJECT}_{cluster_nr_downscaling}.",
-                                                          s_list,".gdx")))["LandCover_G4MID"]
-      } else {
-
-        out_aux <-  gdx(path(CD,WD_DOWNSCALING,"gdx", str_glue("{GDX_OUTPUT_NAME}_{PROJECT}_{cluster_nr_downscaling}.",
-                                                               s_list,".gdx")))["LandCover_G4MID"]
-        out <- rbind(out,out_aux)
-
-      }
-
-    }
-
-    # Write merged gdx file to G4M data folder
-    write.gdx(path(CD, PATH_FOR_G4M, str_glue("downscaled_output_{PROJECT}_{DATE_LABEL}.gdx")),
-              list(LandCover_G4MID=out))
-
-  }
-}
-
-#' Function to transfer the downscaling output to G4M folder. Merges the downscaled
-#' regions if required
+#' Transfer the downscaling output to G4M folder.
 #'
 #' @param cluster_nr_downscaling Cluster sequence number of prior downscaling HTCondor submission
 merge_and_transfer <- function(cluster_nr_downscaling) {
@@ -88,17 +43,20 @@ merge_and_transfer <- function(cluster_nr_downscaling) {
       if (i==1) {
         # Write csv file
         write_csv(downs2, str_glue(CD,"/{WD_G4M}/Data/GLOBIOM/{PROJECT}_{DATE_LABEL}/GLOBIOM2G4M_output_LC_abs_{PROJECT}_{DATE_LABEL}.csv"),
-                  quote = F, col_names = T)
+                  col_names = T)
       } else {
         # Append to csv file
         write_csv(downs2, str_glue(CD,"/{WD_G4M}/Data/GLOBIOM/{PROJECT}_{DATE_LABEL}/GLOBIOM2G4M_output_LC_abs_{PROJECT}_{DATE_LABEL}.csv"),
-                  quote = F, append = T)
+                  append = T)
       }
 
 
     }
 
   }
+
+  # Save global environment
+  save_environment("g4m")
 
 }
 
@@ -123,7 +81,7 @@ compile_g4m_data <- function(baseline = NULL) {
     file_suffix <- str_glue("_{PROJECT}_{DATE_LABEL}_") # for now in future should be indexed by project and date label
 
     # G4M scenario ID
-    g4m_jobs <- get_g4m_jobs_new(baseline = baseline)[-1]
+    g4m_jobs <- get_g4m_jobs(baseline = baseline)[-1]
 
     # Split dimensions and extract scenario name
     scenarios_split <- str_split_fixed(g4m_jobs," ",n=4)
