314178e5dc1095be1324d12453b80dc45345da2b
andreyaugustynczik <augustynczik@iiasa.ac.at>
2021-11-02 11:30:19 +0100

    Correct G4M output file generation

--- R/transfer.R
+++ R/transfer.R
@@ -4,12 +4,31 @@
 #'
 #' @param cluster_nr_downscaling Cluster sequence number of prior downscaling HTCondor submission
 merge_and_transfer <- function(cluster_nr_downscaling) {
+
+  save_environment("3a")
+
   # Transfer gdx to G4M folder - in case files were merged on limpopo
   if (MERGE_GDX_DOWNSCALING){
 
-    # Save merged output to G4M folder
-    f <- str_glue(WD_DOWNSCALING,"/gdx/downscaled_{PROJECT}_{cluster_nr_downscaling}_merged.gdx")
-    file_copy(f, path(CD, PATH_FOR_G4M, str_glue("downscaled_output_{PROJECT}_{DATE_LABEL}.gdx")), overwrite = TRUE)
+    # Get downscaling output
+    downs_files <- rgdx.param(path(CD,WD_DOWNSCALING,"gdx",
+                                   str_glue("{GDX_OUTPUT_NAME}_{PROJECT}_{cluster_nr_downscaling}_merged.gdx")),"LandCover_G4MID")
+
+    # Select data for G4M
+    if (dim(downs_files)[2] == 8) downs_files <- downs_files[,-1]
+
+    downs_files <- subset(downs_files, downs_files[,5] == "Reserved")
+    downs_files <- downs_files[,-5]
+
+    names(downs_files) <- c("g4m_05_id","SCEN1","SCEN3","SCEN2","Year","value")
+
+    # Remap years to columns
+    downs2 <- downs_files %>% spread(Year, value, fill = 0, convert = FALSE)
+
+    # Save to G4M folder
+    write_csv(downs2, str_glue(CD,"/{WD_G4M}/Data/GLOBIOM/{PROJECT}_{DATE_LABEL}/GLOBIOM2G4M_output_LC_abs_{PROJECT}_{DATE_LABEL}.csv"),
+              col_names = T)
+
   } else {
 
     # Get scenario mapping and indices
@@ -24,17 +43,15 @@ merge_and_transfer <- function(cluster_nr_downscaling) {
 
       # Read data for G4M
 
-      downs_files <- gdx(path(CD,WD_DOWNSCALING,"gdx", str_glue("{GDX_OUTPUT_NAME}_{PROJECT}_{cluster_nr_downscaling}.",
-                                                                s_list,".gdx")))["LandCover_G4MID"]
+      downs_files <- rgdx.param(path(CD,WD_DOWNSCALING,"gdx", str_glue("{GDX_OUTPUT_NAME}_{PROJECT}_{cluster_nr_downscaling}.",
+                                                                    s_list,".gdx")),"LandCover_G4MID")
 
-      if (dim(downs_files)[2] == 8){
-        downs_files <- downs_files[,-1]
-        downs_files <- subset(downs_files, V6 == "Reserved")
-      } else {
-        downs_files <- subset(downs_files, V5 == "Reserved")
-      }
+      # Select data for G4M
+      if (dim(downs_files)[2] == 8) downs_files <- downs_files[,-1]
 
+      downs_files <- subset(downs_files, downs_files[,5] == "Reserved")
       downs_files <- downs_files[,-5]
+
       names(downs_files) <- c("g4m_05_id","SCEN1","SCEN3","SCEN2","Year","value")
 
       # Remap years to columns
@@ -56,7 +73,7 @@ merge_and_transfer <- function(cluster_nr_downscaling) {
   }
 
   # Save global environment
-  save_environment("g4m")
+  save_environment("3b")
 
 }
 
@@ -73,6 +90,10 @@ compile_g4m_data <- function(baseline = NULL) {
   prior_wd <- getwd()
   rc <- tryCatch ({
     setwd(WD_G4M)
+
+    # Create folder to store temporary output files
+    aux_out_path <- path(CD,WD_G4M,"out",str_glue("{PROJECT}_{DATE_LABEL}/outfiles"))
+    if (!dir_exists(aux_out_path))  dir_create(aux_out_path)
 
     # Path of output folder
     file_path <- path_wd("out", str_glue("{PROJECT}_{DATE_LABEL}"))
@@ -96,13 +117,9 @@ compile_g4m_data <- function(baseline = NULL) {
     # Compile results
     generate_g4M_report(file_path, file_suffix, scenarios, scenario_names, N, co2)
 
-    # Edit and save csv file for GAMS
-    g4m_out <- read.csv(path(file_path, G4M_FEEDBACK_FILE))
-    colnames(g4m_out)[1:3] <- ""
-    colnames(g4m_out) <- str_replace_all(colnames(g4m_out),"X","")
-    write.csv(g4m_out, path(file_path, G4M_FEEDBACK_FILE), row.names = F, quote = T)
   },
   finally = {
     setwd(prior_wd)
+    save_environment("5b")
   })
 }
