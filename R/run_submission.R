7ca2842f117d51efa85d94008a57a59e4c54ac5e
andreyaugustynczik <augustynczik@iiasa.ac.at>
2021-11-03 14:58:50 +0100

    Add function to submit post-processing to limpopo

--- R/run_submission.R
+++ R/run_submission.R
@@ -286,3 +286,165 @@ run_g4m <- function(baseline = NULL) {
   if (rc != 0) stop("Condor run failed!")
   if (baseline) {save_environment("4")} else {save_environment("5a")}
 }
+
+
+
+#' Run final post-processing
+#'
+#' Reads, edits and executes the GLOBIOM 8_merged_output_tmp.gms script to generate
+#' reports for IAMC.
+#'
+#' @param cluster_nr_globiom Cluster sequence number of prior GLOBIOM HTCondor submission
+run_final_postproc_limpopo <- function(cluster_nr_globiom) {
+
+  # Create a tmp copy of the merge output file with a tmp $include
+  tempString <- read_lines(path(WD_GLOBIOM, "Model", "8_merge_output.gms"))
+  if (!any(str_detect(tempString,"8c_rep_iamc_g4m_tmp.gms"))) tempString <- string_replace(tempString,"\\$include\\s+8c_rep_iamc_g4m.gms","$include 8c_rep_iamc_g4m_tmp.gms")
+  write_lines(tempString, path(WD_GLOBIOM, "Model", "8_merge_output_tmp.gms"))
+
+  # Construct path for feedback file
+  path_feedback <- str_glue(".%X%output%X%g4m%X%{PROJECT}_{DATE_LABEL}%X%")
+  path_feedback_aux <- path(CD,WD_G4M,"out",str_glue("{PROJECT}_{DATE_LABEL}"),G4M_FEEDBACK_FILE)
+
+  # rGet G4M scenario list
+  scen_map <-  subset(unique(get_mapping()[1,-4]), ScenLoop %in% SCENARIOS_FOR_G4M)
+  length_scen1 <- length(unlist(str_split(scen_map[1],"_")))
+  length_scen2 <- length(unlist(str_split(scen_map[3],"_")))
+  length_scen3 <- length(unlist(str_split(scen_map[2],"_")))
+
+  # Define G4M scenarios
+  scen <- matrix(unlist(str_split(get_g4m_jobs(baseline = FALSE)[-1]," ")),ncol=4,byrow=T)[,3]
+  scen <- scen[which(scen != "")]
+
+  # Split G4M scenarios into GLOBIOM dimensions
+  scen_aux <- str_split(scen,"_")
+  scen_aux <- matrix(unlist(scen_aux),ncol = length(scen_aux[[1]]), byrow = TRUE)
+  scen_globiom_map <- array(dim=c(length(scen),3),data="")
+  for(i in 1:dim(scen_aux)[1]){
+    scen_globiom_map[i,1] <- do.call(str_glue, c(as.list(scen_aux[i,1:length_scen1]), .sep = "_"))
+    scen_globiom_map[i,2] <- do.call(str_glue, c(as.list(scen_aux[i,(length_scen1 + 1):(length_scen1 + length_scen2)]), .sep = "_"))
+    scen_globiom_map[i,3] <- do.call(str_glue, c(as.list(scen_aux[i,c(-1:-(length_scen1 + length_scen2))]), .sep = "_"))
+  }
+
+
+  # Check if scenario name must be treated as string
+  special_char <- FALSE
+  if (any(str_detect(scen,"%"))) special_char <- TRUE
+  if (special_char)  scen <- unlist(lapply(scen,function(x) str_glue("\"",x,"\"")))
+
+  # Define column indices of Macro, Bioen and IEA scenarios
+  macro_idx <- which(str_detect(scen_globiom_map[1,],regex("SSP",ignore_case = T)))
+  iea_idx <- which(str_detect(scen_globiom_map[1,],regex("RCP",ignore_case = T)))
+  bioen_idx <- 6 - macro_idx - iea_idx
+
+  if(special_char) scen_globiom_map[,bioen_idx] <- unlist(lapply(scen_globiom_map[,bioen_idx],function(x) str_glue("\"",x,"\"")))
+
+  # Create scenario mapping string
+  for (i in 1:length(scen)) {
+    if (i == 1) {
+      map_string <-
+        str_glue(scen[i],
+                 " . ",
+                 scen_globiom_map[i, macro_idx],
+                 " . ",
+                 scen_globiom_map[i, bioen_idx],
+                 " . ",
+                 scen_globiom_map[i, iea_idx])
+    } else {
+      map_string <-
+        c(
+          map_string,
+          str_glue(
+            scen[i],
+            " . ",
+            scen_globiom_map[i, macro_idx],
+            " . ",
+            scen_globiom_map[i, bioen_idx],
+            " . ",
+            scen_globiom_map[i, iea_idx]
+          )
+        )
+    }
+  }
+
+  # Define sets for mapping
+  g4m_globiom_map <- c("G4MScen2","/",scen,"/","","G4M_SCEN_MAP(G4MScen2,*,*,*)",
+                       "/",map_string,"/",";")
+
+  # Edit mapping set
+  tempString <- read_file(path(WD_GLOBIOM, "Model", "8c_rep_iamc_g4m.gms"))
+  tempString <- string_replace(tempString,regex('G4MScen2[[:print:]*|[\r\n]*]*G4M_SCEN_MAP[[:print:]*|[\r\n]*]*/[\r\n\\s]+;'),
+                               str_c(g4m_globiom_map,collapse="\n"))
+
+  # Edit feedback file
+  path_for_feedback_file <- str_glue(path_feedback,G4M_FEEDBACK_FILE)
+
+  tempString <- string_replace(tempString,"\\$include\\s+[:print:]*X[:print:]*",
+                               str_glue('$include "{path_for_feedback_file}"'))
+
+  # Edit baseline scenario for forest management GHG accounting
+  ref_sum <-  str_glue("- G4M_SCENOUTPUT_DATA(REGION,\"{BASE_SCEN1}\",\"{BASE_SCEN2}\",\"{BASE_SCEN3}\",\"em_fm_bm_mtco2year\",ScenYear)")
+  ref <-  "-\\s+G4M_SCENOUTPUT_DATA[:print:]REGION,[:print:]+,\"em_fm_bm_mtco2year\",ScenYear[:print:]"
+
+  tempString <- string_replace_all(tempString,regex(ref, ignore_case = T),
+                                   ref_sum)
+
+  write_lines(tempString, path(CD,WD_GLOBIOM,"Model","8c_rep_iamc_g4m_tmp.gms"))
+
+  # Save edits and run post-processing script in the GLOBIOM Model directory
+  prior_wd <- getwd()
+  rc <- tryCatch ({
+
+    # Define configuration template
+    config_template <- c(
+      'EXPERIMENT = "{PROJECT}"',
+      'PREFIX = "_globiom"',
+      'JOBS = 0',
+      'HOST_REGEXP = "^limpopo"',
+      'REQUEST_MEMORY = 200000',
+      'REQUEST_CPUS = 1',
+      'GAMS_CURDIR = "Model"',
+      'GAMS_FILE_PATH = "{GLOBIOM_POST_FILE}"',
+      'GAMS_VERSION = "32.2"',
+      'GAMS_ARGUMENTS = "//limpopo={LIMPOPO_RUN} //limpopo_nr={cluster_nr_globiom} //project={PROJECT} //lab={DATE_LABEL} //rep_g4m={REPORTING_G4M_FINAL} //rep_iamc_glo={REPORTING_IAMC_FINAL} //rep_iamc_g4m={REPORTING_IAMC_G4M_FINAL} //g4mfile={G4M_FEEDBACK_FILE} //regionagg={REGIONAL_AG} //nsim=%1"',
+      'BUNDLE_INCLUDE = "Model"',
+      'BUNDLE_INCLUDE_DIRS = c("include")',
+      'BUNDLE_EXCLUDE_DIRS = c(".git", ".svn", "225*", "doc")',
+      'BUNDLE_EXCLUDE_FILES = c("**/*.~*", "**/*.log", "**/*.log~*", "**/*.lxi", "**/*.lst")',
+      'G00_OUTPUT_DIR = "t"',
+      'G00_OUTPUT_FILE = "a8_out.g00"',
+      'GET_G00_OUTPUT = FALSE',
+      'GDX_OUTPUT_DIR = "output/iamc/"',
+      'GDX_OUTPUT_FILE = "Output4_IAMC_template_{PROJECT}_{DATE_LABEL}.gdx"',
+      'GET_GDX_OUTPUT = TRUE',
+      'MERGE_GDX_OUTPUT = FALSE',
+      'WAIT_FOR_RUN_COMPLETION = TRUE',
+      'CLEAR_LINES = FALSE'
+    )
+
+    config_path <- path(TEMP_DIR, "config_glob.R")
+
+    # Write config file
+    current_env <- environment()
+    write_lines(lapply(config_template, .envir=current_env, str_glue), config_path)
+    rm(config_template, current_env)
+
+    # Submit GLOBIOM scenarios and wait for run completion
+    rc <- tryCatch ({
+      setwd(WD_GLOBIOM)
+      system(str_glue("Rscript --vanilla {CD}/Condor_run_R/Condor_run.R {config_path}"))
+    },
+    finally = {
+      setwd(prior_wd)
+    })
+    if (rc != 0) stop("Condor run failed!")
+
+  },
+  finally = {
+    setwd(prior_wd)
+
+    # Clean global environment files
+    clear_environment()
+  })
+
+}
