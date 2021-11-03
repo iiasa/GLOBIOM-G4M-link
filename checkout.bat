200da28fb639ce009f9075d9c1affaedd1857c35
Albert Brouwer <brouwer@iiasa.ac.at>
2021-11-03 17:29:02 +0100

    Reference latest Trunk commit 3554
    Also convert line endings to CR+LF for cross-platform compatibility with trailing hashes to ignore the CR at the end of bash lines.

--- checkout.bat
+++ checkout.bat
@@ -1,7 +1,7 @@
 :<<BATCH
 @echo off
 pushd %~dp0
-svn checkout --revision 3468 svn://hpg909/svn_globiom/globiom_live/Trunk GLOBIOM
+svn checkout --revision 3554 svn://hpg909/svn_globiom/globiom_live/Trunk GLOBIOM
 popd
 exit /b :: end batch script processing
 BATCH
@@ -11,5 +11,5 @@ BATCH
 # the trailing # makes the shell ignore the CR. Must be run with the bash shell.
 pushd "$(dirname "$0")" #
 set -e #
-svn checkout --revision 3468 svn://hpg909/svn_globiom/globiom_live/Trunk GLOBIOM #
+svn checkout --revision 3554 svn://hpg909/svn_globiom/globiom_live/Trunk GLOBIOM #
 popd #
