8ee00ec23171fa9c4e13ce0c53105bde64113005
Yazhen Wu <wuyazhen@pku.edu.cn>
2025-07-05 12:11:35 +0200

    Update default.R
    Update default choice and comment for the parameter SCENARIOS_BTC

--- R/configuration/default.R
+++ R/configuration/default.R
@@ -19,7 +19,7 @@ GLOBIOM_GAMS_ARGS = "//nsim=%1 //limpopo=yes //yes_output=1 //lookup=0 //ssp=SSP
 GLOBIOM_SCEN_FILE = "6_scenarios.gms"
 GLOBIOM_POSTPROC_FILE = "8_merge_output.gms"
 THERE_ARE_BTC_Scenarios = TRUE ## in the modelled scenarios, are there scenarios that include BTC (bending the curve) type of biodiversity protection & conservation setups? (This information needs to be passed on to DownScale to choose the correct initial LC map, and to G4M to prepare corresponding forest protection layers, to ensure consistent and full BTC representation throughout the GLOBIOM-G4M pipeline)
-SCENARIOS_BTC <- c(0,1,8,9,10)
+SCENARIOS_BTC <- c(0) # Change this scenario list, to include only scenarios that include the BTC setting. If all scenarios do not have BTC setting, then put SCENARIOS_BTC <- NULL.
 
 #-------------------------------------------------------------------------------
 
